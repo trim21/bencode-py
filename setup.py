285293fd46797151e165c4b3661906fe537f53a4
Trim21 <trim21.me@gmail.com>
2024-04-14 22:19:13 +0800

    bump: v0.0.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ extensions = [
 
 setup(
     name="bencode2",
-    version="0.0.7",
+    version="0.0.8",
     description="bencode serialize/deserialize library",
     long_description=get_readme(),
     long_description_content_type="text/markdown",
