98487c1e1c05e439908a067c879f60a77d6e9b67
Trim21 <trim21.me@gmail.com>
2024-04-14 14:05:19 +0800

    build: project meta

--- setup.py
+++ setup.py
@@ -2,21 +2,7 @@ from setuptools import setup
 
 from Cython.Build import cythonize
 
-
-def get_readme():
-    """Load README.rst for display on PyPI."""
-    with open('readme.md') as f:
-        return f.read()
-
-
 setup(
-    name='bencode2',
-    version='0.0.2',
-    description='bencode serialize/deserialize library',
-    long_description=get_readme(),
-    long_description_content_type='text/markdown',
-    author='trim21',
-    author_email='trim21.me@gmail.com',
     url='https://github.com/trim21/py-bencode',
     package_dir={'bencode2': 'bencode2'},
     packages=['bencode2'],
