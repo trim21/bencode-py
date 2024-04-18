2495ab1b9db78a73ffe9518a2cc1fdde14b71f9f
Trim21 <trim21.me@gmail.com>
2024-04-18 17:54:42 +0800

    fix

--- bench.py
+++ bench.py
@@ -5,11 +5,11 @@ from bencode2 import bdecode
 
 root = pathlib.Path(__file__).parent
 
-torrent_1_raw = root.joinpath("tests/fixtures/56507.torrent").read_bytes()
+torrent_1_raw = root.joinpath("tests/fixtures/56507.torrent.bin").read_bytes()
 torrent_1_data = bdecode(torrent_1_raw)
 
 torrent_2_raw = root.joinpath(
-    "tests/fixtures/ubuntu-22.04.2-desktop-amd64.iso.torrent"
+    "tests/fixtures/ubuntu-22.04.2-desktop-amd64.iso.torrent.bin"
 ).read_bytes()
 torrent_2_data = bdecode(torrent_2_raw)
 
