aa4de4c1419d1021aedb5b5ad2d74ffa39cde846
Trim21 <trim21.me@gmail.com>
2024-04-21 14:25:00 +0800

    bump: 0.0.14

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 setup(
     name="bencode2",
-    version="0.0.13",
+    version="0.0.14",
     description="A fast and correct bencode serialize/deserialize library",
     long_description=get_readme(),
     long_description_content_type="text/markdown",
