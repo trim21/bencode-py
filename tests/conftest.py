26ef7cebd5053e7d84982388df88afd8e8a2a9b3
Trim21 <trim21.me@gmail.com>
2024-06-09 16:48:25 +0800

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,23 @@
+from __future__ import annotations
+
 import bencode2
 
 
-def pytest_configure():
-    if bencode2.COMPILED:
-        print("[test] using compiled bencode2")
+def pytest_addoption(parser):
+    parser.addoption("--assert-pkg-compiled", action="store", default=None)
+
+
+def pytest_configure(config):
+    assert_pkg_compiled: str | None = config.option.assert_pkg_compiled
+    if assert_pkg_compiled is None:
+        return
+
+    if assert_pkg_compiled == "true":
+        assert bencode2.COMPILED
+    elif assert_pkg_compiled == "false":
+        assert not bencode2.COMPILED
     else:
-        print("[test] using non-compiled bencode2")
+        raise ValueError(
+            f"unexpected --assert-pkg-compiled option, should be true/false,"
+            f" got {assert_pkg_compiled!r} instead"
+        )
