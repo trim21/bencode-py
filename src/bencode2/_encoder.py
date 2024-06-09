6018cb4d2e557f25fa08e7430a6be6665584273e
Trim21 <trim21.me@gmail.com>
2024-06-09 18:51:37 +0800

    update

--- src/bencode2/_encoder.py
+++ src/bencode2/_encoder.py
@@ -93,7 +93,7 @@ def __encode(value: Any, r: io.BytesIO, seen: set[int]) -> None:
         return
 
     if isinstance(value, enum.Enum):
-        return __encode(value, value.value, seen)
+        return __encode(value.value, r, seen)
 
     raise TypeError(f"type '{type(value)!r}' not supported by bencode")
 
