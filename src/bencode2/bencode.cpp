098fd8049788d36b7961789047d88db8fdd23eda
Trim21 <trim21.me@gmail.com>
2024-10-12 00:56:52 +0800

    fix ref cnt

--- src/bencode2/bencode.cpp
+++ src/bencode2/bencode.cpp
@@ -4,17 +4,20 @@
 
 namespace py = pybind11;
 
-// py::object dataclasses_fields;
-// py::object is_dataclasses;
+py::object dataclasses_fields;
+py::object is_dataclasses;
 
 extern py::bytes bencode(py::object v);
 
 extern py::object bdecode(py::buffer b);
 
 PYBIND11_MODULE(_bencode, m, py::mod_gil_not_used()) {
-  //    auto mod = m.import("dataclasses");
-  //    dataclasses_fields = mod.attr("fields");
-  //    is_dataclasses = mod.attr("is_dataclass");
+  auto mod = m.import("dataclasses");
+  mod.inc_ref();
+  dataclasses_fields = mod.attr("fields");
+  dataclasses_fields.inc_ref();
+  is_dataclasses = mod.attr("is_dataclass");
+  is_dataclasses.inc_ref();
 
   m.def("bdecode", &bdecode, "");
   m.def("bencode", &bencode, "");
