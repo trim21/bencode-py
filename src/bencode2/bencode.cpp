c708f2673686001551ceb03b97682a2706c275b0
Trim21 <trim21.me@gmail.com>
2024-10-11 22:32:13 +0800

    run with nogil

---
+++ src/bencode2/bencode.cpp
@@ -0,0 +1,16 @@
+#include <pybind11/pybind11.h>
+
+#include "common.h"
+
+namespace py = pybind11;
+
+extern py::bytes bencode(py::object v);
+
+extern py::object bdecode(py::buffer b);
+
+PYBIND11_MODULE(_bencode, m, py::mod_gil_not_used()) {
+  m.def("bdecode", &bdecode, "");
+  m.def("bencode", &bencode, "");
+  py::register_exception<DecodeError>(m, "BencodeDecodeError");
+  py::register_exception<EncodeError>(m, "BencodeEncodeError");
+}
