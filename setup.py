c2b6d8d5b486fdd3aaef7a9edc54e3419811390c
Trim21 <trim21.me@gmail.com>
2024-11-11 23:08:01 +0800

    build: cleanup config (#109)

--- setup.py
+++ setup.py
@@ -3,48 +3,42 @@ import sys
 from glob import glob
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-from setuptools import find_packages, setup
+from setuptools import setup
 
-if (
-    os.environ.get("PY_BENCODE2_PURE_PYTHON") == "1"
-    or sys.implementation.name != "cpython"
-):
-    setup(
-        packages=find_packages("src"),
-        package_dir={"": "src"},
-        package_data={"": ["*.pyi", "py.typed"]},
-        include_package_data=True,
-        zip_safe=False,
-    )
+pure_python = any(
+    [
+        sys.implementation.name != "cpython",
+        os.environ.get("PY_BENCODE2_PURE_PYTHON") == "1",
+    ]
+)
+
+macro = [("FMT_HEADER_ONLY", "")]
+
+extra_compile_args = None
+
+# if os.environ.get("BENCODE_CPP_DEBUG") == "1":
+# macro.append(("BENCODE_CPP_DEBUG", "1"))
+
+if sys.platform == "win32":
+    extra_compile_args = ["/utf-8", "/Zc:__cplusplus"]
+
+module = Pybind11Extension(
+    "bencode2.__bencode",
+    sources=sorted(glob("./src/bencode2/*.cpp")),
+    include_dirs=[
+        "./src/bencode2",
+        "./vendor/fmt/include",
+        "./vendor/small_vector/source/include",
+    ],
+    define_macros=macro,
+    extra_compile_args=extra_compile_args,
+    cxx_std=17,
+)
+
+if pure_python:
+    setup()
 else:
-
-    macro = [("FMT_HEADER_ONLY", "")]
-
-    extra_compile_args = None
-    # if os.environ.get("BENCODE_CPP_DEBUG") == "1":
-    # macro.append(("BENCODE_CPP_DEBUG", "1"))
-    if sys.platform == "win32":
-        extra_compile_args = ["/utf-8", "/Zc:__cplusplus"]
-
-    module = Pybind11Extension(
-        "bencode2.__bencode",
-        sources=sorted(glob("./src/bencode2/*.cpp")),
-        include_dirs=[
-            "./src/bencode2",
-            "./vendor/fmt/include",
-            "./vendor/small_vector/source/include",
-        ],
-        define_macros=macro,
-        extra_compile_args=extra_compile_args,
-        cxx_std=17,
-    )
-
     setup(
         ext_modules=[module],
-        packages=find_packages("src"),
-        package_dir={"": "src"},
-        package_data={"": ["*.h", "*.cpp", "*.pyi", "py.typed"]},
-        include_package_data=True,
         cmdclass={"build_ext": build_ext},
-        zip_safe=False,
     )
