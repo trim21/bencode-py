3df5725e33d57c14539353edf475245126d42641
Trim21 <trim21.me@gmail.com>
2024-11-11 22:44:42 +0800

    skip sdist

--- setup.py
+++ setup.py
@@ -12,30 +12,32 @@ pure_python = any(
     ]
 )
 
+macro = [("FMT_HEADER_ONLY", "")]
+
+extra_compile_args = None
+
+# if os.environ.get("BENCODE_CPP_DEBUG") == "1":
+# macro.append(("BENCODE_CPP_DEBUG", "1"))
+
+if sys.platform == "win32":
+    extra_compile_args = ["/utf-8", "/Zc:__cplusplus"]
+
+module = Pybind11Extension(
+    "bencode2.__bencode",
+    sources=sorted(glob("./src/bencode2/*.cpp")),
+    include_dirs=[
+        "./src/bencode2",
+        "./vendor/fmt/include",
+        "./vendor/small_vector/source/include",
+    ],
+    define_macros=macro,
+    extra_compile_args=extra_compile_args,
+    cxx_std=17,
+)
+
 if pure_python:
     setup()
 else:
-    macro = [("FMT_HEADER_ONLY", "")]
-
-    extra_compile_args = None
-    # if os.environ.get("BENCODE_CPP_DEBUG") == "1":
-    # macro.append(("BENCODE_CPP_DEBUG", "1"))
-    if sys.platform == "win32":
-        extra_compile_args = ["/utf-8", "/Zc:__cplusplus"]
-
-    module = Pybind11Extension(
-        "bencode2.__bencode",
-        sources=sorted(glob("./src/bencode2/*.cpp")),
-        include_dirs=[
-            "./src/bencode2",
-            "./vendor/fmt/include",
-            "./vendor/small_vector/source/include",
-        ],
-        define_macros=macro,
-        extra_compile_args=extra_compile_args,
-        cxx_std=17,
-    )
-
     setup(
         ext_modules=[module],
         cmdclass={"build_ext": build_ext},
