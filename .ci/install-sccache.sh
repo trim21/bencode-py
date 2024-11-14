44b673c6ee06093e52e3fcf0a733e2a795434e8a
Trim21 <trim21.me@gmail.com>
2024-11-14 14:31:18 +0800

    fix

--- .ci/install-sccache.sh
+++ .ci/install-sccache.sh
@@ -5,6 +5,6 @@ sccache_version=0.8.2
 cd /tmp
 
 # Install sccache
-wget --progress=dot:mega https://github.com/mozilla/sccache/releases/download/v${sccache_version}/sccache-v${sccache_version}-$(arch)-unknown-linux-musl.tar.gz
+curl -sSL https://github.com/mozilla/sccache/releases/download/v${sccache_version}/sccache-v${sccache_version}-$(arch)-unknown-linux-musl.tar.gz
 tar -zxf sccache-v${sccache_version}-$(arch)-unknown-linux-musl.tar.gz
 cp sccache-v${sccache_version}-$(arch)-unknown-linux-musl/sccache /usr/bin
