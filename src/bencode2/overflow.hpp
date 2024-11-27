ebabf68deafcfbf0a6a5c20ac9bc8493b8c7e5b1
Trim21 <trim21.me@gmail.com>
2024-11-27 19:53:48 +0800

    remove some submodule

--- src/bencode2/overflow.hpp
+++ src/bencode2/overflow.hpp
@@ -2,14 +2,24 @@
 
 #include <stdint.h>
 
-#include <safe-math/safe-math.h>
-
 // some helper to check int operator overflow
 
 static int inline _i64_add_overflow(int64_t a, int64_t b, int64_t *res) {
-    return psnip_safe_int64_add(res, a, b);
+#if defined(__GNUC__) or defined(__clang__)
+    return __builtin_add_overflow(a, b, res);
+#elif defined(_MSC_VER)
+    return _Add_overflow(a, b, res);
+#else
+#error "unknown compiler";
+#endif
 }
 
 static int inline _i64_mul_overflow(int64_t a, int64_t b, int64_t *res) {
-    return psnip_safe_int64_mul(res, a, b);
+#if defined(__GNUC__) or defined(__clang__)
+    return __builtin_mul_overflow(a, b, res);
+#elif defined(_MSC_VER)
+    return _Multiply_overflow(a, b, res);
+#else
+#error "unknown compiler";
+#endif
 }
