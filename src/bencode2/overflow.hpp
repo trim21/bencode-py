4d6b35f9f3445a35a3b13f7d9c60ded38815320c
Trim21 <trim21.me@gmail.com>
2024-11-27 20:39:09 +0800

    fix

--- src/bencode2/overflow.hpp
+++ src/bencode2/overflow.hpp
@@ -12,7 +12,7 @@ static bool inline _i64_add_overflow(int64_t a, int64_t b, int64_t *res) {
 #if defined(__GNUC__) or defined(__clang__)
     return __builtin_add_overflow(a, b, res);
 #elif defined(_MSC_VER)
-    return IntAdd<int64_t, int64_t>(a, b, res);
+    return LongLongAdd(a, b, res);
 #else
 #error "unknown compiler";
 #endif
@@ -22,7 +22,7 @@ static bool inline _i64_mul_overflow(int64_t a, int64_t b, int64_t *res) {
 #if defined(__GNUC__) or defined(__clang__)
     return __builtin_mul_overflow(a, b, res);
 #elif defined(_MSC_VER)
-    return IntMult<int64_t, int64_t>(a, b, res);
+    return LongLongMult(a, b, res);
 #else
 #error "unknown compiler";
 #endif
