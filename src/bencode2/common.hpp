bfac62516eb70f41082502d38c0d1d31b6e1b498
Trim21 <trim21.me@gmail.com>
2024-12-10 21:35:07 +0800

    debug macro

--- src/bencode2/common.hpp
+++ src/bencode2/common.hpp
@@ -8,34 +8,8 @@
 #pragma warning(disable : 4996)
 #endif
 
-#ifdef _MSC_VER
-#define print_log(format, ...)                                                                     \
-                                                                                                   \
-    do {                                                                                           \
-        printf(__FILE__);                                                                          \
-        printf(":");                                                                               \
-        printf("%d", __LINE__);                                                                    \
-        printf("\t%s", __FUNCTION__);                                                              \
-        printf("\tDEBUG: ");                                                                       \
-        fmt::println(format, __VA_ARGS__);                                                         \
-    } while (0)
-
-#else
-
-#define print_log(format, ...)                                                                     \
-    do {                                                                                           \
-        printf(__FILE__);                                                                          \
-        printf(":");                                                                               \
-        printf("%d", __LINE__);                                                                    \
-        printf("\t%s\tDEBUG: ", __PRETTY_FUNCTION__);                                              \
-        fmt::println(format, ##__VA_ARGS__);                                                       \
-    } while (0)
-
-#endif
-
 #ifdef BENCODE_CPP_DEBUG
 
-#ifdef _MSC_VER
 #define debug_print(format, ...)                                                                   \
                                                                                                    \
     do {                                                                                           \
@@ -44,21 +18,9 @@
         printf("%d", __LINE__);                                                                    \
         printf("\t%s", __FUNCTION__);                                                              \
         printf("\tDEBUG: ");                                                                       \
-        fmt::println(format, __VA_ARGS__);                                                         \
-    } while (0)
-
-#else
-
-#define debug_print(format, ...)                                                                   \
-    do {                                                                                           \
-        printf(__FILE__);                                                                          \
-        printf(":");                                                                               \
-        printf("%d", __LINE__);                                                                    \
-        printf("\t%s\tDEBUG: ", __PRETTY_FUNCTION__);                                              \
         fmt::println(format, ##__VA_ARGS__);                                                       \
     } while (0)
 
-#endif
 #else
 
 #define debug_print(...)                                                                           \
