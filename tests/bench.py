de0616ccf90004e4b61953b7077cb4761981fe69
Trim21 <trim21.me@gmail.com>
2024-12-13 04:57:46 +0800

    fix benchmark case

--- tests/bench.py
+++ tests/bench.py
@@ -32,50 +32,50 @@ multiple_files_torrent = (
 )
 
 
-def test_benchmark_cpp_encode_compat_peers_bytes_key(benchmark):
+def test_benchmark_encode_compat_peers_bytes_key_cpp(benchmark):
     benchmark(
         cpp_bencode,
         {key.encode(): value for key, value in compat_peers_py.items()},
     )
 
 
-def test_benchmark_cpp_encode_compat_peers_str_key(benchmark):
+def test_benchmark_encode_compat_peers_str_key_cpp(benchmark):
     benchmark(cpp_bencode, compat_peers_py)
 
 
-def test_benchmark_cpp_decode_compat_peers(benchmark):
+def test_benchmark_decode_compat_peers_cpp(benchmark):
     benchmark(cpp_bdecode, py_bencode(compat_peers_py))
 
 
-def test_benchmark_cpp_decode_single_file_torrent(benchmark):
+def test_benchmark_decode_single_file_torrent_cpp(benchmark):
     benchmark(cpp_bdecode, single_file_torrent)
 
 
-def test_benchmark_cpp_encode_single_file_torrent(benchmark):
+def test_benchmark_encode_single_file_torrent_cpp(benchmark):
     benchmark(cpp_bencode, py_bdecode(single_file_torrent))
 
 
-def test_benchmark_cpp_decode_multiple_files_torrent(benchmark):
+def test_benchmark_decode_multiple_files_torrent_cpp(benchmark):
     benchmark(cpp_bdecode, multiple_files_torrent)
 
 
-def test_benchmark_cpp_encode_multiple_files_torrent(benchmark):
+def test_benchmark_encode_multiple_files_torrent_cpp(benchmark):
     benchmark(cpp_bencode, py_bdecode(multiple_files_torrent))
 
 
-def test_benchmark_py_decode_single_file_torrent(benchmark):
+def test_benchmark_decode_single_file_torrent_py(benchmark):
     benchmark(py_bdecode, single_file_torrent)
 
 
-def test_benchmark_py_encode_single_file_torrent(benchmark):
+def test_benchmark_encode_single_file_torrent_py(benchmark):
     benchmark(py_bencode, py_bdecode(single_file_torrent))
 
 
-def test_benchmark_py_decode_multiple_files_torrent(benchmark):
+def test_benchmark_decode_multiple_files_torrent_py(benchmark):
     benchmark(py_bdecode, multiple_files_torrent)
 
 
-def test_benchmark_py_encode_multiple_files_torrent(benchmark):
+def test_benchmark_encode_multiple_files_torrent_py(benchmark):
     benchmark(py_bencode, py_bdecode(multiple_files_torrent))
 
 
@@ -90,9 +90,9 @@ class AnnounceCompatResponse:
     # "peers6": b"1" * 18 * 50,
 
 
-def test_benchmark_cpp_encode_compact_peers_dataclass(benchmark):
+def test_benchmark_encode_compact_peers_dataclass_cpp(benchmark):
     benchmark(cpp_bencode, AnnounceCompatResponse(**compat_peers_py))
 
 
-def test_benchmark_py_encode_compact_peers_dataclass(benchmark):
+def test_benchmark_encode_compact_peers_dataclass_py(benchmark):
     benchmark(py_bencode, AnnounceCompatResponse(**compat_peers_py))
