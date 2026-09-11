5f3852df85c86519971516d4c6bd673c0b98bd2c
Trim21 <trim21.me@gmail.com>
2026-09-12 01:52:13 +0800

    build: build wheels with pdm-backend and a meson hook (#479)

--- src/bencode2/bencode.cpp
+++ src/bencode2/bencode.cpp
@@ -3,21 +3,20 @@ namespace nb = nanobind;
 
 #include "common.hpp"
 
-// dataclasses.fields
-nb::object dataclasses_fields;
+// dataclasses.fields, leaked on purpose: a static destructor would decref
+// python objects after the interpreter has been finalized
+nb::object *dataclasses_fields = nullptr;
 
 // dataclasses.is_dataclass
-nb::object is_dataclasses;
+nb::object *is_dataclasses = nullptr;
 
 extern nb::bytes bencode(nb::object v);
 extern nb::object bdecode(nb::object b);
 
 NB_MODULE(__bencode, m) {
     auto mod = m.import_("dataclasses");
-    dataclasses_fields = mod.attr("fields");
-    dataclasses_fields.inc_ref();
-    is_dataclasses = mod.attr("is_dataclass");
-    is_dataclasses.inc_ref();
+    dataclasses_fields = new nb::object(mod.attr("fields"));
+    is_dataclasses = new nb::object(mod.attr("is_dataclass"));
 
     nb::exception<EncodeError>(m, "BencodeEncodeError", PyExc_ValueError);
     nb::exception<DecodeError>(m, "BencodeDecodeError", PyExc_ValueError);
